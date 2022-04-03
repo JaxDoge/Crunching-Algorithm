8f3cb10181f695aceca3bc25efbbe96830fea874
JaxDoge <niftyjax@gmail.com>
2022-04-03 20:43:09 +0800

    921

--- Stack/20. Valid Parentheses.py
+++ Stack/20. Valid Parentheses.py
@@ -8,4 +8,20 @@ class Solution:
         n = len(s)
 
         for i in range(n):
-            
+            if s[i] in {'{','[','('}:
+                left_stack.appendleft(s[i])
+            elif len(left_stack) != 0 and self.leftPa(s[i]) == left_stack[0]:
+                left_stack.popleft()
+            else:
+                return False
+
+        # All left parentheses are matched, then return true, otherwise return false
+        return len(left_stack) == 0
+
+
+    def leftPa(self, right_pa):
+        if right_pa == '}':
+            return '{'
+        elif right_pa == ']':
+            return '['
+        return '('
