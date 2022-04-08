1f905b9f5faa0ec44ffc1e7969b730c961afa67d
JaxDoge <niftyjax@gmail.com>
2022-04-08 19:16:06 +0800

    9

---
+++ Array/9. Palindrome Number.py
@@ -0,0 +1,28 @@
+9. Palindrome Number
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        if x < 10:
+            return True
+
+        from collections import deque
+        divisor = 10
+        tmp = x
+        help_stack = deque()
+        while True:
+            help_stack.append(tmp mod divisor)
+            tmp = tmp // divisor
+            if tmp == 0:
+                break
+
+        y = 0
+        while len(help_stack):
+            y = y * 10 + help_stack.popleft()
+
+        return x == y
+
+
+
--- DFA/8. String to Integer (atoi).py
+++ DFA/8. String to Integer (atoi).py
@@ -1,22 +1,29 @@
 8. String to Integer (atoi)
 
 
+# Deterministic Finite Automaton
+
 INT_MAX = 2 ** 31 - 1
 INT_MIN = -2 ** 31
 
 class Automaton:
     def __init__(self):
+        # initial state
         self.state = 'start'
+        # sign
         self.sign = 1
+        # result
         self.ans = 0
+        # transition function
         self.table = {
-            'start': ['start', 'signed', 'in_number', 'end'],
-            'signed': ['end', 'end', 'in_number', 'end'],
-            'in_number': ['end', 'end', 'in_number', 'end'],
-            'end': ['end', 'end', 'end', 'end'],
+            'start':['start', 'signed', 'in_number', 'end'],
+            'signed':['end','end','in_number','end'],
+            'in_number':['end','end','in_number','end'],
+            'end':['end','end','end','end']
         }
-        
+
     def get_col(self, c):
+        # map any character to certain acceptable input
         if c.isspace():
             return 0
         if c == '+' or c == '-':
@@ -26,12 +33,18 @@ class Automaton:
         return 3
 
     def get(self, c):
+        # change state based on c
         self.state = self.table[self.state][self.get_col(c)]
+        # check integer overflow, update ans
         if self.state == 'in_number':
-            self.ans = self.ans * 10 + int(c)
-            self.ans = min(self.ans, INT_MAX) if self.sign == 1 else min(self.ans, -INT_MIN)
-        elif self.state == 'signed':
+            tmp = (INT_MAX - int(c)) / 10 if self.sign == 1 else (-INT_MIN - int(c)) / 10
+            if self.ans >= tmp:
+                self.ans = INT_MAX if self.sign == 1 else -INT_MIN
+            else:
+                self.ans = self.ans * 10 + int(c)
+        if self.state == 'signed':
             self.sign = 1 if c == '+' else -1
+
 
 class Solution:
     def myAtoi(self, str: str) -> int:
@@ -39,4 +52,3 @@ class Solution:
         for c in str:
             automaton.get(c)
         return automaton.sign * automaton.ans
-
