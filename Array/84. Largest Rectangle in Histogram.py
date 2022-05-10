321de87a4ee1da1aeda741271e0a54a6d3d91161
JaxDoge <niftyjax@gmail.com>
2022-05-10 13:59:19 +0800

    84

--- Array/84. Largest Rectangle in Histogram.py
+++ Array/84. Largest Rectangle in Histogram.py
@@ -1,26 +1,34 @@
 84. Largest Rectangle in Histogram
 
 
-
+# Monotonous stack
+# Sentinel
+# Fine the left and right boundary of a rectangle, which has the height of current bar
+from collections import deque
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-
         n = len(heights)
-        left, right = [0] * n, [0] * n
+        left = []  # left boundary of each bar
+        right = deque()  # right boundary of each bar
 
-        mono_stack = list()
+        mono_stack = []
         for i in range(n):
-            while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+            while len(mono_stack) and heights[mono_stack[-1]] >= heights[i]:
                 mono_stack.pop()
-            left[i] = mono_stack[-1] if mono_stack else -1
+
+            leftB = mono_stack[-1] if len(mono_stack) else -1  # -1 is sentinel
+            left.append(leftB)
             mono_stack.append(i)
-        
-        mono_stack = list()
-        for i in range(n - 1, -1, -1):
-            while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+
+        mono_stack = []
+        for i in range(n-1,-1,-1):
+            while len(mono_stack) and heights[mono_stack[-1]] >= heights[i]:
                 mono_stack.pop()
-            right[i] = mono_stack[-1] if mono_stack else n
+
+            rightB = mono_stack[-1] if len(mono_stack) else n  # n is sentinel
+            right.appendleft(rightB)
             mono_stack.append(i)
-        
-        ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
-        return ans
+
+        res = max((right[i] - left[i] - 1) * heights[i] for i in range(n))
+
+        return res
