5e1429a74c91cdcd19824c9a62b5533fd7d91ae9
JaxDoge <niftyjax@gmail.com>
2022-05-24 22:23:26 +0800

    158

---
+++ WTF/158. Read N Characters Given Read4 II - Call multi.py
@@ -0,0 +1,16 @@
+158. Read N Characters Given Read4 II - Call multiple times
+
+
+
+class Solution:
+    def __init__(self):
+        def reader():
+            buf4=['']*4
+            while n:=read4(buf4):
+                yield from buf4[:n]
+        self.it=reader()
+    def read(self, buf: List[str], n: int) -> int:
+        i=-1
+        for i,c in zip(range(n),self.it):
+            buf[i]=c
+        return i+1
