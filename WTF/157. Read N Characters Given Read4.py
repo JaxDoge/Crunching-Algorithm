81f9398fd2325cb03eab45adda0f70feaf4e8d13
JaxDoge <niftyjax@gmail.com>
2022-05-24 21:18:24 +0800

    157

---
+++ WTF/157. Read N Characters Given Read4.py
@@ -0,0 +1,48 @@
+157. Read N Characters Given Read4
+
+
+
+
+
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        res = 0
+        j = 0
+        while True:
+            tmp = [None] * 4
+            m = read4(tmp)
+            if m == 0:
+                break
+
+            for i in range(m):
+                buf[j] = tmp[i]
+                j += 1
+                res += 1
+                if res == n:
+                    break
+                    
+            if res == n:
+                break
+
+
+        return res
