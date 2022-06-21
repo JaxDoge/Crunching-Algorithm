7eb1b174515422719d5c25f9a01084f7149f6a65
JaxDoge <niftyjax@gmail.com>
2022-06-21 21:11:42 +0800

    1114

---
+++ Concurrent/1114. Print in Order.py
@@ -0,0 +1,34 @@
+1114. Print in Order
+
+
+
+from threading import Lock
+
+
+class Foo:
+    def __init__(self):
+        self.firstjob = Lock()
+        self.secondjob = Lock()
+        self.firstjob.acquire()
+        self.secondjob.acquire()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.firstjob.release()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        with self.firstjob:
+            printSecond()
+            self.secondjob.release()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        with self.secondjob:
+            printThird()
