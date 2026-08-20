7420361c7b7e7967e63d3895677aa9b7e93c5c0a
JaxDoge <niftyjax@gmail.com>
2026-08-20 15:05:44 -0700

    152

--- Dynamic Program/152. Maximum Product Subarray.py
+++ Dynamic Program/152. Maximum Product Subarray.py
@@ -5,36 +5,35 @@
 # Note that we need another dp table minDP to record the minimum product of subarray end with nums[i-1]
 # Because nums[i] could be negative, thus it could get a largest value with the minimum values 
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n < 2:
-            return nums[0]
+	def maxProduct(self, nums: List[int]) -> int:
+		n = len(nums)
+		if n < 2:
+			return nums[0]
 
-        maxDP = [nums[0]]
-        minDP = [nums[0]]
-        for i in range(1, n):
-            maxI = max(maxDP[i - 1] * nums[i], minDP[i - 1] * nums[i], nums[i])
-            minI = min(maxDP[i - 1] * nums[i], minDP[i - 1] * nums[i], nums[i])
-            maxDP.append(maxI)
-            minDP.append(minI)
+		maxDP = [nums[0]]
+		minDP = [nums[0]]
+		for i in range(1, n):
+			maxI = max(maxDP[i - 1] * nums[i], minDP[i - 1] * nums[i], nums[i])
+			minI = min(maxDP[i - 1] * nums[i], minDP[i - 1] * nums[i], nums[i])
+			maxDP.append(maxI)
+			minDP.append(minI)
 
-        return max(maxDP)
+		return max(maxDP)
 
 
-# Obviously not this way
-# time limitation exceeded!!
+# I got it
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        n = len(nums)
-        res = float("-inf")
+	def maxProduct(self, nums: List[int]) -> int:
+		n = len(nums)
+		dp_max = [float('-inf')] * (n)
+		dp_min = [float('inf')] * (n)
 
-        for i in range(n):
-            cur = nums[i]
-            for j in range(i, n):
-                if j != i:
-                    cur *= nums[j]
+		dp_max[0] = nums[0]
+		dp_min[0] = nums[0]
 
-                res = max(res, cur)
-
-        return res
+		for i in range(1, n):
+			dp_max[i] = max(dp_max[i-1] * nums[i], dp_min[i-1] * nums[i], nums[i])
+			dp_min[i] = min(dp_max[i-1] * nums[i], dp_min[i-1] * nums[i], nums[i])
+		
+		return max(dp_max)
 
