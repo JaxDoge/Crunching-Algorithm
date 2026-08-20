f04931cb409ab2bb0ccf365dc69294a9b64e80ea
JaxDoge <niftyjax@gmail.com>
2026-08-20 15:10:31 -0700

    brainteasing

--- Array/9. Palindrome Number.py
+++ Array/9. Palindrome Number.py
@@ -1,28 +1,28 @@
 9. Palindrome Number
 
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+	def isPalindrome(self, x: int) -> bool:
+		if x < 0:
+			return False
 
-        if x < 10:
-            return True
+		if x < 10:
+			return True
 
-        from collections import deque
-        divisor = 10
-        tmp = x
-        help_stack = deque()
-        while True:
-            help_stack.append(tmp % divisor)
-            tmp = tmp // divisor
-            if tmp == 0:
-                break
+		from collections import deque
+		divisor = 10
+		tmp = x
+		help_stack = deque()
+		while True:
+			help_stack.append(tmp % divisor)
+			tmp = tmp // divisor
+			if tmp == 0:
+				break
 
-        y = 0
-        while len(help_stack):
-            y = y * 10 + help_stack.popleft()
+		y = 0
+		while len(help_stack):
+			y = y * 10 + help_stack.popleft()
 
-        return x == y
+		return x == y
 
 
 
