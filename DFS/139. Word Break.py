f26de6799d6abdab3b082992f12dfc02186c2c3b
JaxDoge <niftyjax@gmail.com>
2026-08-20 14:43:11 -0700

    word break

--- DFS/139. Word Break.py
+++ DFS/139. Word Break.py
@@ -6,39 +6,55 @@
 # Backtrack
 from collections import defaultdict
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        m = len(wordDict)
+	def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+		n = len(s)
+		m = len(wordDict)
 
-        wordMap = defaultdict(list)
+		wordMap = defaultdict(list)
 
-        for i in range(m):
-            fL = wordDict[i][0]
-            wordMap[fL].append(wordDict[i])
+		for i in range(m):
+			fL = wordDict[i][0]
+			wordMap[fL].append(wordDict[i])
 
-        memo = {}
-        def backtrack(start):
-            nonlocal n, m, wordMap, s
-            # base case
-            if start == n:
-                return True
+		memo = {}
+		def backtrack(start):
+			nonlocal n, m, wordMap, s
+			# base case
+			if start == n:
+				return True
 
-            firstLetter = s[start]
-            if (str(start), firstLetter) in memo:
-                return memo[(str(start), firstLetter)]
+			firstLetter = s[start]
+			if (str(start), firstLetter) in memo:
+				return memo[(str(start), firstLetter)]
 
-            cands = wordMap[firstLetter]
-            res = False
-            for w in cands:
-                span = len(w)
-                targetW = s[start:start+span]
-                if w == targetW:
-                    res = backtrack(start+span)
-                    if res:
-                        break
+			cands = wordMap[firstLetter]
+			res = False
+			for w in cands:
+				span = len(w)
+				targetW = s[start:start+span]
+				if w == targetW:
+					res = backtrack(start+span)
+					if res:
+						break
 
-            memo[(str(start), firstLetter)] = res
-            return res
+			memo[(str(start), firstLetter)] = res
+			return res
 
-        return backtrack(0)
+		return backtrack(0)
 
+# DP solution 1
+class Solution:
+	def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+		word_set = set(wordDict)
+		n = len(s)
+
+		dp = [False] * (n + 1)
+		dp[0] = True
+	
+		for i in range(1, n+1):
+			for j in range(i):
+				if dp[j] and s[j:i] in word_set:
+					dp[i] = True
+					break
+		
+		return dp[n]
--- DFS/140. Word Break II.py
+++ DFS/140. Word Break II.py
@@ -5,41 +5,67 @@
 # OR DP table 
 from collections import defaultdict
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        n = len(s)
-        m = len(wordDict)
-        wordMap = defaultdict(list)
+	def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+		n = len(s)
+		m = len(wordDict)
+		wordMap = defaultdict(list)
 
-        for i in range(m):
-            fL = wordDict[i][0]
-            wordMap[fL].append(wordDict[i])
+		for i in range(m):
+			fL = wordDict[i][0]
+			wordMap[fL].append(wordDict[i])
 
-        memo = {}
-        # backtrack will find all valid path for s[start:]
-        # return list[list]
-        def backtrack(start):
-            nonlocal memo, m, n, s, wordMap
-            # base case
-            if start == n:
-                return [[]]
+		memo = {}
+		# backtrack will find all valid path for s[start:]
+		# return list[list]
+		def backtrack(start):
+			nonlocal memo, m, n, s, wordMap
+			# base case
+			if start == n:
+				return [[]]
 
-            if start in memo:    
-                return memo[start]
+			if start in memo:    
+				return memo[start]
 
 
-            firstL = s[start]
-            curPaths = []
-            for w in wordMap[firstL]:
-                span = len(w)
-                targetW = s[start:start+span]
-                if w == targetW:
-                    # choose w, get the result of s[start+len(w):]
-                    restPaths = backtrack(start+span)
-                    for rp in restPaths:
-                        curPaths.append([w]+rp[:])
+			firstL = s[start]
+			curPaths = []
+			for w in wordMap[firstL]:
+				span = len(w)
+				targetW = s[start:start+span]
+				if w == targetW:
+					# choose w, get the result of s[start+len(w):]
+					restPaths = backtrack(start+span)
+					for rp in restPaths:
+						curPaths.append([w]+rp[:])
 
-            memo[start] = curPaths
+			memo[start] = curPaths
 
-            return curPaths
-        res = backtrack(0)
-        return ["".join(x) for x in res]
+			return curPaths
+		res = backtrack(0)
+		return ["".join(x) for x in res]
+
+
+# DP solution
+class Solution:
+	def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+		words_map = set(wordDict)
+		n = len(s)
+
+		# dp[i] = all sentences that can construct s[:i]
+		dp = [[] for _ in range(n + 1)]
+		dp[0] = [""]
+		
+		for i in range(1, n + 1):
+			for j in range(i):
+				word = s[j:i]
+
+				if word not in words_map:
+					continue
+				
+				for sentence in dp[j]:
+					if sentence:
+						dp[i].append(sentence + ' ' + word)
+					else:
+						dp[i].append(word)
+		
+		return dp[-1]
