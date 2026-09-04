6fbd435f16c381a64b3d679b21b95914e030ebc0
JaxDoge <niftyjax@gmail.com>
2026-09-03 20:41:28 -0700

    28

--- Dynamic Program/28. Implement strStr().py
+++ Dynamic Program/28. Implement strStr().py
@@ -3,26 +3,37 @@
 
 # GO GO GO KMP
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-    	if not needle:
-    		return 0
-    	n = len(haystack)
-    	m = len(needle)
+	def strStr(self, haystack: str, needle: str) -> int:
+		if not needle:
+			return 0
+		n = len(haystack)
+		m = len(needle)
 
-    	dp = [[0]*256 for _ in range(m)]
-    	dp[0][ord(needle[0])] = 1
+		dp = [[0]*256 for _ in range(m)]
+		dp[0][ord(needle[0])] = 1
 
-    	X = 0
-        # Note that j start from index 1
-    	for j in range(1, m):
-    		for c in range(0, 256):
-    			dp[j][c] = dp[X][c]
-    		dp[j][ord(needle[j])] = j + 1
-    		X = dp[X][ord(needle[j])]
+		X = 0
+		# Note that j start from index 1
+		for j in range(1, m):
+			for c in range(0, 256):
+				dp[j][c] = dp[X][c]
+			dp[j][ord(needle[j])] = j + 1
+			X = dp[X][ord(needle[j])]
 
-    	J = 0
-    	for i in range(n):
-    		J = dp[J][ord(haystack[i])]
-    		if J == m:
-    			return i - m + 1
-    	return -1
+		J = 0
+		for i in range(n):
+			J = dp[J][ord(haystack[i])]
+			if J == m:
+				return i - m + 1
+		return -1
+
+
+class Solution:
+	def strStr(self, haystack: str, needle: str) -> int:
+		m = len(haystack)
+		n = len(needle)
+
+		if n > m:
+			return -1
+
+		return haystack.find(needle)
