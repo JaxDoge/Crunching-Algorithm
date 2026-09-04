a24376a741675da3bd3115eed901137f0d691330
JaxDoge <niftyjax@gmail.com>
2026-09-04 12:10:01 -0700

    30

--- Dynamic Program/28. Implement strStr().py
+++ Dynamic Program/28. Implement strStr().py
@@ -37,3 +37,82 @@ class Solution:
 			return -1
 
 		return haystack.find(needle)
+
+
+# Standard KMP
+class Solution:
+	def buildLPS(self, pattern):
+		m = len(pattern)
+		length = 0
+		i = 1
+		lps = [0] * m
+
+		while i < m:
+			# Check if we can extend the lps in substring [...i]
+			if pattern[i] == pattern[length]:
+				length += 1
+				lps[i] = length
+				i += 1
+			else:
+				# Don't fall all the way back to the beginning of pattern
+				# Fallback and reuse previous match length
+				if length != 0:
+					length = lps[length - 1]
+				else:
+					lps[i] = 0
+					i += 1
+
+		return lps
+
+	def strStr(self, haystack: str, needle: str) -> int:
+		n = len(haystack)
+		m = len(needle)
+		if n < m:
+			return -1
+
+		lps = self.buildLPS(needle)
+
+		i = 0
+		j = 0
+
+		while i < n:
+			if haystack[i] == needle[j]:
+				i += 1
+				j += 1
+
+				if j == m:
+					# j = lps[j - 1]
+					return i - m
+
+			else:
+				if j != 0:
+					j = lps[j - 1]
+
+				else:
+					i += 1
+
+		return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
