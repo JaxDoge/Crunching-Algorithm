97c5e941254c876259ab98f52afe9ccdbc4d29e4
JaxDoge <niftyjax@gmail.com>
2026-09-04 21:40:24 -0700

    34

--- Array/34. Find First and Last Position of Element in Sorted Array.py
+++ Array/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,41 +1,25 @@
 34. Find First and Last Position of Element in Sorted Array
 
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-    	if not nums: return [-1,-1]
-    	length = len(nums)
-    	
-    	def findLeft(left, right):
-    		nonlocal nums, target
-    		while left <= right:
-    			mid = left+(right-left)//2
-    			if nums[mid] == target:
-    				right = mid-1
-    			elif nums[mid] > target:
-    				right = mid-1
-    			elif nums[mid] < target:
-    				left = mid+1
-    		if left >= len(nums) or nums[left] != target:
-    			return -1
-    		return left
+	def searchRange(self, nums: List[int], target: int) -> List[int]:
+		n = len(nums)
+		if not n:
+			return [-1, -1]
 
-    	def findRight(left, right):
-    		nonlocal nums, target
-    		while left <= right:
-    			mid = left+(right-left)//2
-    			if nums[mid] == target:
-    				left = mid+1
-    			elif nums[mid] > target:
-    				right = mid-1
-    			elif nums[mid] < target:
-    				left = mid+1
-    		if right < 0 or nums[right] != target:
-    			return -1
-    		return right
+		def lower_bound(x):
+			left, right = 0, n - 1
+			while left <= right:
+				mid = (left + right) // 2
+				if nums[mid] >= x:
+					right = mid - 1
+				elif nums[mid] < x:
+					left = mid + 1
 
-    	left_bound = findLeft(0,length-1)
-    	if left_bound == -1:
-    		return [-1,-1]
-    	else:
-    		right_bound = findRight(0,length-1)
-    		return [left_bound, right_bound]
+			return left
+
+		start = lower_bound(target)
+		if start == n or nums[start] != target:
+			return [-1, -1]
+		
+		end = lower_bound(target + 1)
+		return [start, end - 1]
