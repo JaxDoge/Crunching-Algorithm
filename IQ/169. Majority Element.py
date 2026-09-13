d2d9961351302332d0442331de375e44b845e4c4
JaxDoge <niftyjax@gmail.com>
2026-09-13 10:48:26 -0700

    2 qs

--- IQ/169. Majority Element.py
+++ IQ/169. Majority Element.py
@@ -8,29 +8,29 @@
 
 2）假设candidate不等于a，则当count从1变为0的过程， 此区间内a的数量小于等于其余数的数量，因此以count=0为分界线，数组右端部分的众数仍然为a
 
-因此，以count=0可以将整个原始数组分为若干部分，count=0右端部分的数组中的众数永远是a，最终必然会筛选出a
+因此，以count=0可以将整个原始数组分为若干部分，count=0右端部分的数组中的众数永远是a，最终必然会筛选出a (count 最后一定大于 0)
 '''
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        cand = float("inf")
-        count = 0 
+	def majorityElement(self, nums: List[int]) -> int:
+		n = len(nums)
+		cand = float("inf")
+		count = 0 
 
-        for i in range(n):
-            if count == 0:
-                cand = nums[i]
-            
-            if cand == nums[i]:
-                count += 1
-            else:
-                count -= 1
+		for i in range(n):
+			if count == 0:
+				cand = nums[i]
+			
+			if cand == nums[i]:
+				count += 1
+			else:
+				count -= 1
 
-        return cand
+		return cand
 
 
 
 # Divide Conquer
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
+	def majorityElement(self, nums: List[int]) -> int:
