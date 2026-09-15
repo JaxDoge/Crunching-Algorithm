3aea584d26b0f19706574a706c2b1f6fb8425a8a
JaxDoge <niftyjax@gmail.com>
2026-09-15 14:26:11 -0700

    66

--- Array/66. Plus One.py
+++ Array/66. Plus One.py
@@ -4,24 +4,24 @@
 
 from collections import deque
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        res = deque()
-        carry = 1
-        n = len(digits)
+	def plusOne(self, digits: List[int]) -> List[int]:
+		res = deque()
+		carry = 1
+		n = len(digits)
 
-        for i in range(n-1, -1, -1):
-            d = digits[i]
-            if carry > 0:
-                d_sum = d + carry
-                if d_sum > 9:
-                    res.appendleft(d_sum % 10)
-                else:
-                    res.appendleft(d_sum)
-                    carry = 0
-            else:
-                res.appendleft(d)
+		for i in range(n-1, -1, -1):
+			d = digits[i]
+			if carry > 0:
+				d_sum = d + carry
+				if d_sum > 9:
+					res.appendleft(d_sum % 10)
+				else:
+					res.appendleft(d_sum)
+					carry = 0
+			else:
+				res.appendleft(d)
 
-        if carry > 0:
-            res.appendleft(1)
+		if carry > 0:
+			res.appendleft(1)
 
-        return list(res)
+		return list(res)
