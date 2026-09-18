53f1b923b79ddd06d43ac28006452553f0111b6d
JaxDoge <niftyjax@gmail.com>
2026-09-18 14:59:30 -0700

    89

--- IQ/89. Gray Code.py
+++ IQ/89. Gray Code.py
@@ -2,50 +2,58 @@
 
 
 # Backtrak
-from collections import Counter
 class Solution:
-    def grayCode(self, n: int) -> List[int]:
-        memo = {0}  # record picked num in ten digits
-        res = [0]
-        
+	def grayCode(self, n: int) -> list[int]:
+		seq_len = 2 ** n
+		seen = set()
+		res = []
+		def dfs(path, cur_num):
+			# Successfully find a sequence
+			if len(path) == seq_len:
+				if cur_num == 0:
+					res.extend(path)
+				return
+			
+			# cur_num already in path
+			if cur_num in seen:
+				return
+			
+			# Add cur_num in path
+			seen.add(cur_num)
+			path.append(cur_num)
 
-        def backtrack():
-            nonlocal memo, res, n
-            # base case, when the length of res is n and the last element is valid
-            if len(res) == 2 ** n:
-                htXOR = res[0] ^ res[-1]
-                counter = Counter("{0:b}".format(htXOR))
-                if counter["1"] == 1:
-                    return True
+			# Change one bit
+			for i in range(n):
+				mask = 1 << i
+				cand = cur_num ^ mask
+				dfs(path, cand)
+				if res: break
 
-                return False
+			path.pop()
+			seen.remove(cur_num)
 
-            flag = False
-            for i in range(n):
-                medium = "0" * i + "1" + "0" * (n - i - 1)
-                cand = res[-1] ^ int(medium, 2)
-                if cand not in memo:
-                    res.append(cand)
-                    memo.add(cand)
-                    flag = backtrack()
-                    if flag:
-                        break
-                    res.pop()
-                    memo.remove(cand)
-
-            return flag
-
-        backtrack()
-        return res
+		dfs([], 0)
+		return res
 
 
 
 # Formula
 # https://baike.baidu.com/item/%E6%A0%BC%E9%9B%B7%E7%A0%81
 class Solution:
-    def grayCode(self, n: int) -> List[int]:
-        ans = [0] * (1 << n)
-        for i in range(1 << n):
-            ans[i] = (i >> 1) ^ i
-        return ans
+	def grayCode(self, n: int) -> List[int]:
+		ans = [0] * (1 << n)
+		for i in range(1 << n):
+			ans[i] = (i >> 1) ^ i
+		return ans
 
+
+class Solution:
+	def grayCode(self, n: int) -> list[int]:
+		res = [0]
+
+		for i in range(n):
+			mask = 1 << i
+			for j in range(len(res) - 1, -1, -1):
+				res.append(res[j] | mask)
+
+		return res
