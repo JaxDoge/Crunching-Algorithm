0949f1db2620b9e06ed1b3b9a904acb55730e3d4
JaxDoge <niftyjax@gmail.com>
2026-09-19 09:42:51 -0700

    90

--- Array/90. Subsets II.py
+++ Array/90. Subsets II.py
@@ -3,29 +3,29 @@
 
 
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
+	def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+		nums = sorted(nums)
 
-        res = []
-        cur_path = []
+		res = []
+		cur_path = []
 
-        def backtrack(start):
-            nonlocal res, cur_path, nums
-            # Preorder traverl
-            res.append(cur_path.copy())
-            if start == len(nums):
-                return
+		def backtrack(start):
+			nonlocal res, cur_path, nums
+			# Preorder traverl
+			res.append(cur_path.copy())
+			if start == len(nums):
+				return
 
-            for i in range(start, len(nums)):
-                # Pruning tree
-                if i > start and nums[i] == nums[i - 1]:
-                    continue
+			for i in range(start, len(nums)):
+				# If the current element is a duplicate, ignore.
+				if i > start and nums[i] == nums[i - 1]:
+					continue
 
-                cur_path.append(nums[i])
-                backtrack(i + 1)   # Caution !!
-                cur_path.pop()
+				cur_path.append(nums[i])
+				backtrack(i + 1)   # Caution !!
+				cur_path.pop()
 
-            return
+			return
 
-        backtrack(0)
-        return res
+		backtrack(0)
+		return res
