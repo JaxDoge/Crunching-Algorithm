3df92b86a1fbd726f2a6a88ea45bddcf46a98351
JaxDoge <niftyjax@gmail.com>
2026-09-22 11:00:32 -0700

    96

--- 二叉树/96. Unique Binary Search Trees.py
+++ 二叉树/96. Unique Binary Search Trees.py
@@ -7,49 +7,70 @@
 
 
 class Solution:
-    def numTrees(self, n: int) -> int:
-        import collections
-    	memo = collections.defaultdict(int)
-    	def counter(n: int):
-    		nonlocal memo
-    		if n <= 1:   # Only one or None node, there is one result
-    		    return 1
-            
-            res = 0
-    		for i in range(1,n+1):
-    			# Check the memo if the left subtree counter exists
-    			if memo[i-1]:
-    				left_num = memo[i-1]
-    			else:   #  Has previous result, read it
-    				left_num = counter(i-1)
-    				memo[i-1] = left_num
+	def numTrees(self, n: int) -> int:
+		import collections
+		memo = collections.defaultdict(int)
+		def counter(n: int):
+			nonlocal memo
+			if n <= 1:   # Only one or None node, there is one result
+				return 1
+			
+			res = 0
+			for i in range(1,n+1):
+				# Check the memo if the left subtree counter exists
+				if memo[i-1]:
+					left_num = memo[i-1]
+				else:   #  Has previous result, read it
+					left_num = counter(i-1)
+					memo[i-1] = left_num
 
-    			# Same the memo if the left subtree counter exists
-    			if memo[n-i]:
-    				right_num = memo[n-i]
-    			else:
+				# Same the memo if the left subtree counter exists
+				if memo[n-i]:
+					right_num = memo[n-i]
+				else:
 
 					right_num = counter(n-i)
-    				memo[n-i] = right_num
+					memo[n-i] = right_num
 
-    		    res += left_num * right_num
-            return res 
+				res += left_num * right_num
+			return res 
 
-        return counter(n)
+		return counter(n)
 
+class Solution:
+	def numTrees(self, n: int) -> int:
+		memo = {}
+		def build(low, high):
+			
+			if low > high:
+				return 1
+			
+			if high - low in memo:
+				return memo[high - low]
+
+			tree_cnt = 0
+			for root_val in range(low, high + 1):
+				left_tree_cnt = build(low, root_val - 1)
+				right_tree_cnt = build(root_val + 1, high)
+				tree_cnt += left_tree_cnt * right_tree_cnt
+			
+			memo[high-low] = tree_cnt
+			return tree_cnt
+		
+		return build(1, n)
 
 
 
 # 卡塔兰级数，没什么可说的，背公式吧
 
 class Solution(object):
-    def numTrees(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        C = 1
-        for i in range(0, n):
-            C = C * 2*(2*i+1)/(i+2)
-        return int(C)
+	def numTrees(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		C = 1
+		for i in range(0, n):
+			C = C * 2*(2*i+1)/(i+2)
+		return int(C)
 
