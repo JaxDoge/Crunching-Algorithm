bcec64cb3ba84769dfd0a4c5d61b6a974b835479
JaxDoge <niftyjax@gmail.com>
2026-09-24 10:43:54 -0700

    103

--- 二叉树/103. Binary Tree Zigzag Level Order Traversal.py
+++ 二叉树/103. Binary Tree Zigzag Level Order Traversal.py
@@ -12,38 +12,36 @@
 
 # 层序遍历
 class Solution:
-    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-    	if not root: return []
-    	
-    	from collections import deque
-    	res = []
-    	helper = deque()
-    	helper.append(root)
-    	zigzag_flag = True
+	def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+		if not root: return []
+		
+		from collections import deque
+		res = []
+		helper = deque()
+		helper.append(root)
+		zigzag_flag = True
 
-    	while helper:
-    		sub_list = []
-    		sub_queue_size = len(helper)  # 每层遍历节点数
-    		for i in range(sub_queue_size):
-    			if zigzag_flag:
-    			    ele = helper.popleft()
-				    sub_list.append(ele.val)
-				    if ele.left:
-				    	helper.append(ele.left)
-				    if ele.right:
-				    	helper.append(ele.right)
+		while helper:
+			sub_list = []
+			sub_queue_size = len(helper)  # 每层遍历节点数
+			for i in range(sub_queue_size):
+				if zigzag_flag:
+					ele = helper.popleft()
+					sub_list.append(ele.val)
+					if ele.left:
+						helper.append(ele.left)
+					if ele.right:
+						helper.append(ele.right)
 				else:
-                	ele = helper.pop()
-                    sub_list.append(ele.val)
-                    # 入栈的顺序，左右也要反过来！！
-                    if ele.right:
-                    	helper.appendleft(ele.right)
-                    if ele.left:
-                    	helper.appendleft(ele.left)
+					ele = helper.pop()
+					sub_list.append(ele.val)
+					# 入栈的顺序，左右也要反过来！！
+					if ele.right:
+						helper.appendleft(ele.right)
+					if ele.left:
+						helper.appendleft(ele.left)
 
-
-    	        i += 1
-            res.append(sub_list)
-    	    # 改变标识符方向
-            zigzag_flag = not zigzag_flag
-        return res
+			res.append(sub_list)
+			# 改变标识符方向
+			zigzag_flag = not zigzag_flag
+		return res
