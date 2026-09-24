5bed866ab1c1ec4c92d80fb34cc6f0266a912bcc
JaxDoge <niftyjax@gmail.com>
2026-09-24 10:49:06 -0700

    104

--- 二叉树/104. Maximum Depth of Binary Tree.py
+++ 二叉树/104. Maximum Depth of Binary Tree.py
@@ -10,9 +10,33 @@
 
 
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        if not root: 
-            return 0
-        left_height = self.maxDepth(root.left)
-        right_height = self.maxDepth(root.right)
-        return max(left_height,right_height) + 1
+	def maxDepth(self, root: TreeNode) -> int:
+		if not root: 
+			return 0
+		left_height = self.maxDepth(root.left)
+		right_height = self.maxDepth(root.right)
+		return max(left_height,right_height) + 1
+
+
+
+class Solution:
+	def maxDepth(self, root: TreeNode | None) -> int:
+		if not root:
+			return 0
+
+		queue = deque()
+		queue.append(root)
+		res = 0
+
+		while queue:
+			cur_level_size = len(queue)
+
+			for i in range(cur_level_size):
+				node = queue.popleft()
+				if node.left:
+					queue.append(node.left)
+				if node.right:
+					queue.append(node.right)
+			res += 1
+
+		return res
