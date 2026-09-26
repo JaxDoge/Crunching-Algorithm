6e5a49151049678b76cce21f09cf8216eaf3b6fa
JaxDoge <niftyjax@gmail.com>
2026-09-26 10:04:05 -0700

    111

--- 二叉树/111. Minimum Depth of Binary Tree.py
+++ 二叉树/111. Minimum Depth of Binary Tree.py
@@ -10,43 +10,40 @@
 
 # 递归解法
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
-    	def lowestSubtree(node):
-    		if not node: return 0
-    		left_height = lowestSubtree(node.left)
-    		right_height = lowestSubtree(node.right)
-            # 需要考虑特殊情况，某个子树为空树，必然高度最小，但却不能返回这个子树的高度
-    		if not left_height: return right_height + 1
-    		if not right_height: return left_height +1
-    		return min(left_height,right_height)+1
+	def minDepth(self, root: TreeNode) -> int:
+		def lowestSubtree(node):
+			if not node: return 0
+			left_height = lowestSubtree(node.left)
+			right_height = lowestSubtree(node.right)
+			# 需要考虑特殊情况，某个子树为空树，必然高度最小，但却不能返回这个子树的高度
+			if not left_height: return right_height + 1
+			if not right_height: return left_height +1
+			return min(left_height,right_height)+1
 
-    	return lowestSubtree(root)
+		return lowestSubtree(root)
 
 # 层序遍历解法
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
-        if not root: return 0
-        import collections
-        node_queue = collections.deque()
-        # traver_list = []                 # Maybe we don't need it
-        height = 1             # Record current travesal height, start from one
-        node_queue.append(root)
+	def minDepth(self, root: TreeNode | None) -> int:
+		res = 0
+		if not root:
+			return res
+		
+		queue = deque([root])
 
-        while node_queue:
-            sub_loop_num = len(node_queue)    # Record the number of nodes in this layer
+		while queue:
+			res += 1
+			size = len(queue)
+			for _ in range(size):
+				node = queue.popleft()
+				if not node.left and not node.right:
+					return res
+				if node.left:
+					queue.append(node.left)
+				if node.right:
+					queue.append(node.right)
 
-            for i in range(sub_loop_num):
-                pop_node = node_queue.popleft()
-                if pop_node.left:
-                    node_queue.append(pop_node.left)
-                if pop_node.right:
-                    node_queue.append(pop_node.right)
-                if not pop_node.left and not pop_node.right:  # Found a leaf, it's the first leaf from left
-                    return height
-
-            height += 1
-        return height
-
+		return res
 
 
 
